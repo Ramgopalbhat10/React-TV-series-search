5a4be06ddd37bc81d52ceec43476040575338251
DESKTOP-B6HIVER\Ram <ram.gopal.madumbu@accenture.com>
2018-05-25 11:17:14 +0530

    Project structure and basic setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+import Intro from '../intro/intro';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">TV Series List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Intro message="Here you can find all of your most loved TV series"/>
       </div>
     );
   }
